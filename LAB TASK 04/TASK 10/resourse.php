df26829f443137b4cb80aae973d6454da3a550b4
FtzNusratB <78098184+FtzNusratB@users.noreply.github.com>
2021-03-07 12:45:36 +0600

    Add files via upload

---
+++ LAB TASK 04/TASK 10/resourse.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Full Page</title>
+</head>
+<body>
+<header>
+<span style="color:green;font-weight:bold;font-size:400%">X</span><sub style="fontweight:bold;fontsize:400%">company</sub>
+<h1><a style="font-size:150%;" href="">HOME | </a></h1>
+<h2><a style="font-size:150%;" href="B--Registration.php"> Registration</a></h2>
+<h3><a style="font-size:150%;" href="C--Login.php">Login | </a></h3>
+
+</header>
+</body>
+</html> 
+
